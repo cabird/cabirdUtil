820fedc7509c0ddfc18bf5af7dfd810030417e19
REDMOND\cbird <cbird@microsoft.com>
2018-11-02 11:59:12 -0700

    Initial Commit

---
+++ cabirdUtil/DictionaryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cabirdUtil
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key) where TValue : class, new()
+        {
+            if (dict.TryGetValue(key, out TValue value))
+            {
+                return value;
+            }
+
+            TValue newValue = new TValue();
+            dict[key] = newValue;
+            return newValue;
+        }
+    }
+}
---
+++ cabirdUtil/LinqExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cabirdUtil
+{
+    public static class LinqExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action(item);
+            }
+        }
+    }
+}
